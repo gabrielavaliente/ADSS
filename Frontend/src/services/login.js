a6a63fb82ed83dd0f836be0c43d52e02d73f968e
gabrielavaliente <105072947+gabrielavaliente@users.noreply.github.com>
2023-12-04 11:46:36 -0600

    Update login.js

--- Frontend/src/services/login.js
+++ Frontend/src/services/login.js
@@ -1,33 +1,22 @@
 //const ENDPOINT = 'http://localhost:3501/api'
 // https://apricot-chameleon-wrap.cyclic.app/
 const BASE_URL = `${import.meta.env.VITE_API_URL}api/`;
+import axios from "axios";
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}api`;
 
-function login({ identifier, password }) {
-  console.log("Submitting login form...");
-  return fetch(`${import.meta.env.VITE_API_URL}api/auth/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ identifier, password }),
-  })
-    .then((res) => {
-      if (!res.ok) {
-        console.error("Error:", res.status, res.statusText);
-        throw new Error("Response is not good");
-      }
-      return res.json();
-    })
-    .then((res) => {
-      console.log("Received response:", res);
-      const { token } = res;
-      console.log("Received JWT:", token);
-      return token;
-    })
-    .catch((error) => {
-      console.error("Login error:", error.message);
-      throw error;
-    });
+export const login = async ({identifier,password})=>{
+  try{
+    const response = await axios.post("/auth/login/",{identifier,password});
+    if(response.status===200){
+      return response.data;
+    }else{
+      return 404;
+    }
+    
+  }catch(error){
+    console.log(error);
+  }
 }
+
 
 export default login;
