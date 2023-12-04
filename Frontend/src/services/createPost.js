86ce051f17f89bbb769e9ecc772706de3aa05a3a
gabrielavaliente <105072947+gabrielavaliente@users.noreply.github.com>
2023-12-04 12:36:13 -0600

    Update createPost.js

--- Frontend/src/services/createPost.js
+++ Frontend/src/services/createPost.js
@@ -1,4 +1,6 @@
+const BASE_URL = `${import.meta.env.VITE_API_URL}api/`;
 import axios from "axios";
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}api`;
 
 const createPost = async (postData, jwt) => {
   if (!jwt) {
