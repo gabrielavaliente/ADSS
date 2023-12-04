f7751e60ff9a78c2eac3ae4244b074e94f470ea9
gabrielavaliente <105072947+gabrielavaliente@users.noreply.github.com>
2023-12-04 13:18:27 -0600

    Update createPost.js

--- Frontend/src/services/createPost.js
+++ Frontend/src/services/createPost.js
@@ -1,11 +1,13 @@
-const BASE_URL = `${import.meta.env.VITE_API_URL}api/`;
 import axios from "axios";
+
 axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}api`;
 
-export const createPost = async (postData, jwt) => {
-  try {
-    console.log("JWT Token:", jwt);
+const createPost = async (postData, jwt) => {
+  if (!jwt) {
+    throw new Error("No hay un token JWT válido");
+  }
 
+  try {
     const response = await axios.post("/post", postData, {
       headers: {
         "Content-Type": "application/json",
@@ -13,18 +15,12 @@ export const createPost = async (postData, jwt) => {
       },
     });
 
-    console.log("Response Data:", response.data);
-
-    if (response.status === 201) {
-      return response.data;
-    } else {
-      return 404;
-    }
+    return response.data;
   } catch (error) {
     console.error("Error al crear el post:", error.message);
-    console.error("Axios Error Object:", error);
     throw error;
   }
 };
 
 export default createPost;
+
