239a6c680687d80e5c2b03a2dc930c5418d025ae
mariodiaz7 <105059118+mariodiaz7@users.noreply.github.com>
2023-12-04 12:17:39 -0600

    Update createPost.js

--- Frontend/src/services/createPost.js
+++ Frontend/src/services/createPost.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = `${import.meta.env.VITE_API_URL}api/`;
+
 const createPost = async (postData, jwt) => {
   if (!jwt) {
     throw new Error("No hay un token JWT válido");
   }
 
   try {
-    const response = await axios.post("/post", postData, {
+    const response = await axios.post(`${BASE_URL}post`, postData, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${jwt}`,
@@ -14,12 +16,11 @@ const createPost = async (postData, jwt) => {
     });
 
     // Assuming a successful response has a specific property (e.g., success)
-    if (response.data.success) {
+    if (response.status === 200) {
       return response.data;
     } else {
       // Handle other response scenarios as needed
-      const errorMessage =
-        response.data.message || "Error desconocido al crear el post";
+      const errorMessage = response.data.message || "Error desconocido al crear el post";
       throw new Error(`Error al crear el post: ${errorMessage}`);
     }
   } catch (error) {
