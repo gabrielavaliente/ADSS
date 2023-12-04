fcf98d823286ca37f8113594b562c29a230452ba
gabrielavaliente <105072947+gabrielavaliente@users.noreply.github.com>
2023-12-03 23:57:56 -0600

    Update createProduct.js

--- Frontend/src/services/createProduct.js
+++ Frontend/src/services/createProduct.js
@@ -1,30 +1,31 @@
 const createProduct = async (productData, jwt) => {
-    if (!jwt) {
-        throw new Error("No hay un token JWT válido");
+  if (!jwt) {
+    throw new Error("No hay un token JWT válido");
+  }
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}api/product`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      const errorMessage =
+        errorResponse.message || "Error desconocido al crear el producto";
+      throw new Error(`Error al crear el producto: ${errorMessage}`);
     }
 
-    try {
-        const response = await fetch("http://localhost:3501/api/product", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${jwt}`,
-            },
-            body: JSON.stringify(productData),
-        });
-
-        if (!response.ok) {
-            const errorResponse = await response.json();
-            const errorMessage = errorResponse.message || "Error desconocido al crear el producto";
-            throw new Error(`Error al crear el producto: ${errorMessage}`);
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error al crear el producto:", error.message);
-        throw error;
-    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al crear el producto:", error.message);
+    throw error;
+  }
 };
 
 export default createProduct;
