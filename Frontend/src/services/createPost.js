52ee529d33366b5ee321a8972200e58bfa813b43
gabrielavaliente <105072947+gabrielavaliente@users.noreply.github.com>
2023-12-03 23:57:42 -0600

    Update createPost.js

--- Frontend/src/services/createPost.js
+++ Frontend/src/services/createPost.js
@@ -1,32 +1,31 @@
 const createPost = async (postData, jwt) => {
-    if (!jwt) {
-      throw new Error("No hay un token JWT válido");
+  if (!jwt) {
+    throw new Error("No hay un token JWT válido");
+  }
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}api/post`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      const errorMessage =
+        errorResponse.message || "Error desconocido al crear el post";
+      throw new Error(`Error al crear el post: ${errorMessage}`);
     }
-  
-    try {
-      const response = await fetch("http://localhost:3501/api/post", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${jwt}`,
-        },
-        body: JSON.stringify(postData),
-      });
-  
-      if (!response.ok) {
-        const errorResponse = await response.json();
-        const errorMessage = errorResponse.message || "Error desconocido al crear el post";
-        throw new Error(`Error al crear el post: ${errorMessage}`);
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error al crear el post:", error.message);
-      throw error;
-    }
-  };
-  
-  export default createPost;
-  
-  
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al crear el post:", error.message);
+    throw error;
+  }
+};
+
+export default createPost;
