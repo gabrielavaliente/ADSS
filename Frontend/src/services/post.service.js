5fef364f778d0322e34bf11070938b36d4a89279
gabrielavaliente <105072947+gabrielavaliente@users.noreply.github.com>
2023-12-03 23:59:25 -0600

    Update post.service.js

--- Frontend/src/services/post.service.js
+++ Frontend/src/services/post.service.js
@@ -1,71 +1,66 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3501/api/"
-
+const BASE_URL = `${import.meta.env.VITE_API_URL}api/`;
 
 //Craer pupilaje
 export const createPost = async (formData) => {
-    try {
-      const response = await axios.post(`${BASE_URL}/post`, formData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      return response.data;
-    } catch (error) {
-      console.error("Error al realizar la petición:", error);
-      throw new Error(
-        "Error occurred while creating the post. Please try again."
-      );
-    }
-  };
+  try {
+    const response = await axios.post(`${BASE_URL}/post`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
+    return response.data;
+  } catch (error) {
+    console.error("Error al realizar la petición:", error);
+    throw new Error(
+      "Error occurred while creating the post. Please try again."
+    );
+  }
+};
 
-  //Encontrar todos los pupilajes
-  export const getAllPost = async () => {
-    try {
-      const response = await axios.get(`${BASE_URL}/post`);
-      // console.log(response); // DEBUG
-  
-      if (response.status === 200) return response.data.data;
-      else return [];
-    } catch (error) {
-      console.log("Error:", error.message);
-      return [];
-    }
-  };
+//Encontrar todos los pupilajes
+export const getAllPost = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/post`);
+    // console.log(response); // DEBUG
 
+    if (response.status === 200) return response.data.data;
+    else return [];
+  } catch (error) {
+    console.log("Error:", error.message);
+    return [];
+  }
+};
 
-  // Obtener un pupilaje por id 
+// Obtener un pupilaje por id
 export const getOnePost = async (pupilaje_id) => {
-    try {
-      if (!pupilaje_id) throw new Error("pupilaje_id is required!");
-  
-      const response = await axios.get(`${BASE_URL}/post/${pupilaje_id}`);
-      // console.log(response);
-  
-      if (response.status === 200) return response.data.data;
-      else return undefined;
-    } catch (error) {
-      console.log("Error:", error.message);
-      return undefined;
-    }
-  };
+  try {
+    if (!pupilaje_id) throw new Error("pupilaje_id is required!");
 
-  // Borrar un pupilaje por id 
-  export const deleteOnePost = async (pupilaje_id) => {
-    try {
-      if (!pupilaje_id) throw new Error("pupilaje_id is required!");
-  
-      const response = await axios.delete(`${BASE_URL}/post/${pupilaje_id}`);
-      // console.log(response);
-  
-      if (response.status === 200) return true;
-      else return undefined;
-    } catch (error) {
-      console.log("Error:", error.message);
-      return undefined;
-    }
-  };
-  
-  
+    const response = await axios.get(`${BASE_URL}/post/${pupilaje_id}`);
+    // console.log(response);
+
+    if (response.status === 200) return response.data.data;
+    else return undefined;
+  } catch (error) {
+    console.log("Error:", error.message);
+    return undefined;
+  }
+};
+
+// Borrar un pupilaje por id
+export const deleteOnePost = async (pupilaje_id) => {
+  try {
+    if (!pupilaje_id) throw new Error("pupilaje_id is required!");
+
+    const response = await axios.delete(`${BASE_URL}/post/${pupilaje_id}`);
+    // console.log(response);
+
+    if (response.status === 200) return true;
+    else return undefined;
+  } catch (error) {
+    console.log("Error:", error.message);
+    return undefined;
+  }
+};
